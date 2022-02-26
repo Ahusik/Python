db30bb39ca6c7a86812c435aaf933a9edc77b4bc
Bartosz Adamczuk <bartosz.adamczuk@gmail.com>
2022-02-26 11:02:58 +0100

    dodanie zmiennej a

--- test.py
+++ test.py
@@ -1,3 +1,26 @@
 import sys
+import math
 
 print(sys.version)
+a = 'isi\ngrupa niestacjonarna'
+print(a)
+print(type(a))
+b = 5
+print(b)
+c = 5, 4
+print(c)
+d = 3+2j
+print(d)
+print(type(d))
+
+#nazwa_zmiennej = wartosc
+
+e = 7
+f = 3
+print(e // f)
+print(e ** f)
+#g = 'wizualizacja danych'
+#h = ' grupa 1'
+#
+#print(g+h)
+print(math.pow(e, f))
