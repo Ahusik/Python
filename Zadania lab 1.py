06e22e5b98f1462c234b8272d5640887306046e7
Ahusik <93037845+Ahusik@users.noreply.github.com>
2022-03-11 17:44:50 +0100

    Create Zadania lab 1.py

---
+++ Zadania lab 1.py
@@ -0,0 +1,114 @@
+import sys
+import math
+
+#Zad 1
+# a = 3
+# b = 5
+# print(a + b)
+# c = -6.5
+# print(a + c)
+#
+# zmienna1 = 'Python'
+# zmienna2 = "super"
+# print(zmienna1 + ' jest ' + zmienna2)
+#
+# zmienna1 = input('Podaj imię: ')
+# print('Witaj', zmienna1)
+#
+# zmienna1 = input("Podaj 1 liczbę: ")
+# zmienna2 = input("Podaj 2 liczbę: ")
+# wynik = int(zmienna1) + int(zmienna2)
+# print("Suma:", wynik)
+
+#Zad2
+# print("Kalkulator.\n")
+# a = input("wprowadz pierwsza liczbe: ")
+# b = input("wprowadz drugą liczbe: ")
+# a = int(a)
+# b = int(b)
+# print(" 1. dodawanie\n 2. odejmowanie\n 3. mnożenie\n 4. dzielenie")
+# zmienna1= input("wybierz dzialanie:")
+# if (int(zmienna1) == 1):
+#     dodawanie = a + b
+#     print("wynik dodawania to ", dodawanie)
+# if (int(zmienna1) == 2):
+#     odejmowanie = a - b
+#     print("wynik odejmowania to ", odejmowanie)
+# if (int(zmienna1) == 3):
+#     mnozenie = a * b
+#     print("wynik mnozenia to ", mnozenie)
+# if (int(zmienna1) == 4):
+#     dzielenie = a / b
+#     print("wynik dzielenia to ", dzielenie)
+
+#Zad3
+# a = 3
+# a += 1
+# print(a)
+# b = 3
+# b -= 1
+# print(b)
+# c = 3
+# c *= 1
+# print(c)
+# d = 3
+# d /= 1
+# print(d)
+# e = 3
+# e **= 1
+# print(e)
+# f = 3
+# f %= 1
+# print(f)
+
+#Zad4
+# print("e do potęgi 10")
+# a= input("wprowadz pierwszą liczbe: ")
+# a= int(a)
+# e= a ** 10
+# print(e)
+# NIEDOKONCZONE
+
+#Zad5
+#zmienna1 = 'MATEUSZ'
+#zmienna2 = "WITKOWSKI"
+#print(str.capitalize(zmienna1) + " " + str.capitalize(zmienna2))
+
+#Zad6
+# str = """""Get up on the floor
+# Dancing all night long
+# Get up on the floor
+# Dancing til the break of dawn
+# Get up on the floor
+# Dancing til the break of dawn
+# Get up on the floor
+# Dancing"""""
+# print(str.count("Get up on the floor"))
+
+#Zad7
+#NIEDKONCZONE
+
+#Zad8
+# str = """""Get up on the floor
+#  Dancing all night long
+#  Get up on the floor
+#  Dancing til the break of dawn
+#  Get up on the floor
+#  Dancing til the break of dawn
+#  Get up on the floor
+#  Dancing"""""
+# print(str.split("Dancing"))
+
+#Zad9
+# a = 10
+# print(type(a))
+# print(float(b))
+# a = '10'
+# print(type(a))
+# print(a)
+# b = 10
+# print(type(b))
+# print(hex(b))
+# c = float(10)
+# print(type(c))
+# print(c)
