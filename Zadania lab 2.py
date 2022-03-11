a8b0238b0f7d88ce04bdd8bea32cae42b4498820
Ahusik <93037845+Ahusik@users.noreply.github.com>
2022-03-11 19:49:30 +0100

    Create Zadania lab 2.py

---
+++ Zadania lab 2.py
@@ -0,0 +1,87 @@
+import sys
+import math
+
+#Zad1
+# a = int(input('ile masz ulubionych sportow? '))
+# lista=[]
+# b=0
+# while a > b:
+#     print('podaj nazwe sportu',b+1, ': ')
+#     i = input()
+#     lista.append(i)
+#     b=b+1
+# lista.reverse()
+# c = int(input('ile masz mniej lubianych sportow? '))
+# d=0
+# while c > d:
+#     print('podaj nazwe sportu',d+1, ': ')
+#     i = input()
+#     lista.append(i)
+#     d=d+1
+# print('Twoje sporty: ')
+# for x in lista[::1]:
+#     print(x)
+
+#Zad2
+# slownik = {'nr':'numer',
+# 'poz':'pozycja',
+# 'art':'artykuł',
+# 'ust':'ustęp',
+# 'pkt':'punkt',
+# 'lit':'litera',
+# 'tir':'tiret'}
+# print(len(slownik))
+
+#Zad3
+# Nazwa Gry:Ilość Przegranych Godzin
+# Gry = {
+# 'League of Legends':3200,
+# 'CS:GO':1700,
+# 'Lost Ark':30,
+# 'Dying Light':27,
+# 'Minecraft':1000
+#     }
+# print(Gry)
+# print(len(Gry))
+
+#Zad4
+# a = input('Wpisz słowo a policzę ile w nim jest litery \"a\": ')
+# b=0
+# for x in a[::1]:
+#     if x == 'a':
+#         b = b +1
+# print(b)
+
+#Zad5
+#NIEDOKONCZONE
+
+#Zad6
+#NIEDOKONCZONE
+
+#Zad7
+# lista1 = [4, 2, 6, 10, 0]
+# lista2 = []
+# for x in lista1[::1]:
+#     x = x**2
+#     lista2.append(x)
+# for x in lista2[::1]:
+#     print(x)
+
+#Zad8
+# a = 10
+# lista1 = []
+# lista2 = []
+# while a > 0:
+#     i = int(input('podaj liczbe: '))
+#     lista1.append(i)
+#     if i % 2 == 0:
+#         lista2.append(i)
+#     a = a-1
+# print('lista: ')
+# for x in lista1[::1]:
+#     print(x)
+# print('lista z parzystymi liczbami: ')
+# for x in lista2[::1]:
+#     print(x)
+#Zad9
+#NIEDOKONCZONE
